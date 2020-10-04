c83e704845f8db46ef5080a0f88bc72178208368
rockingatgithub <contactwithsid@gmail.com>
2020-10-04 17:46:54 +0530

    styles added

--- src/components/App.js
+++ src/components/App.js
@@ -5,10 +5,8 @@ import {
   Container,
   Row,
   Navbar,
-  Table,
   Dropdown,
   DropdownButton,
-  Card,
   Button,
 } from "react-bootstrap";
 import SudokuTable from "./SudokuTable";
@@ -16,7 +14,7 @@ import SudokuTable from "./SudokuTable";
 class App extends Component {
   constructor(props) {
     super(props);
-    const easy = [
+    this.easy = [
       [3, 1, 6, 5, 7, 8, 4, 9, 2],
       [5, 2, 9, 1, 3, 4, 7, 6, 8],
       [4, 8, 7, 6, 2, 9, 5, 3, 1],
@@ -27,7 +25,32 @@ class App extends Component {
       [6, 9, 2, 3, 5, 1, 8, 7, 4],
       [7, 4, 5, 2, 8, 6, 3, 1, 9],
     ];
-    let question = [
+
+    this.medium = [
+      [6, 5, 1, 8, 7, 3, 2, 9, 4],
+      [7, 4, 3, 2, 5, 9, 1, 6, 8],
+      [9, 8, 2, 1, 6, 4, 3, 5, 7],
+      [1, 2, 5, 4, 3, 6, 8, 7, 9],
+      [4, 3, 9, 5, 8, 7, 6, 1, 2],
+      [8, 6, 7, 9, 1, 2, 5, 4, 3],
+      [5, 7, 8, 3, 9, 1, 4, 2, 6],
+      [2, 1, 6, 7, 4, 8, 9, 3, 5],
+      [3, 9, 4, 6, 2, 5, 7, 8, 1],
+    ];
+
+    this.question2 = [
+      [6, 5, 0, 8, 7, 3, 0, 9, 0],
+      [0, 0, 3, 2, 5, 0, 0, 0, 8],
+      [9, 8, 0, 1, 0, 4, 3, 5, 7],
+      [1, 0, 5, 0, 0, 0, 0, 0, 0],
+      [4, 0, 0, 0, 0, 0, 0, 0, 2],
+      [0, 0, 0, 0, 0, 0, 5, 0, 3],
+      [5, 7, 8, 3, 0, 1, 0, 2, 6],
+      [2, 0, 0, 0, 4, 8, 9, 0, 0],
+      [0, 9, 0, 6, 2, 5, 0, 8, 1],
+    ];
+
+    this.question = [
       [3, 0, 6, 5, 0, 8, 4, 0, 0],
       [5, 2, 0, 0, 0, 0, 0, 0, 0],
       [0, 8, 7, 0, 0, 0, 0, 3, 1],
@@ -38,18 +61,30 @@ class App extends Component {
       [0, 0, 0, 0, 0, 0, 0, 7, 4],
       [0, 0, 5, 2, 0, 6, 3, 0, 0],
     ];
+
     this.state = {
-      answerGrid: easy,
-      questionGrid: question,
+      answerGrid: this.easy,
+      questionGrid: this.question,
       time: 0,
       start: 0,
     };
   }
 
   handleEasy = () => {
-    const { easy } = this.state;
+    let self = this;
     this.setState({
-      answerGrid: easy,
+      answerGrid: self.easy,
+      questionGrid: this.question,
+      time: 0,
+      start: 0,
+    });
+  };
+
+  handleMedium = () => {
+    let self = this;
+    this.setState({
+      answerGrid: self.medium,
+      questionGrid: this.question2,
       time: 0,
       start: 0,
     });
@@ -68,54 +103,54 @@ class App extends Component {
   };
 
   stopGame = () => {
+    clearInterval(this.timer);
     this.setState({
       time: 0,
       start: 0,
     });
-    clearInterval(this.timer);
 
     return;
   };
 
   render() {
     return (
-      <div>
-        <Container>
-          <Row>
-            <Col>
-              <Navbar bg="light" expand="lg">
-                <Navbar.Brand href="#home">React-Sudoku</Navbar.Brand>
-              </Navbar>
-            </Col>
-          </Row>
-          <Row>
-            <Col>
-              <DropdownButton id="dropdown-basic-button" title="Select Level">
-                <Dropdown.Item href="#" onClick={this.handleEasy}>
-                  Easy
-                </Dropdown.Item>
-                <Dropdown.Item href="#" onClick={this.handleMedium}>
-                  Medium
-                </Dropdown.Item>
-                <Dropdown.Item href="#" onClick={this.handleHard}>
-                  Hard
-                </Dropdown.Item>
-              </DropdownButton>
-              <Card>
-                <Card.Body>Timer: {ms(this.state.time)}</Card.Body>
-              </Card>
-              <Button onClick={this.startGame}>Start</Button>
-              <Table striped bordered hover responsive>
-                <SudokuTable
-                  questionGrid={this.state.questionGrid}
-                  answerGrid={this.state.answerGrid}
-                  stopTimer={this.stopGame}
-                />
-              </Table>
-            </Col>
-          </Row>
-        </Container>
-      </div>
+      <Container>
+        <Row>
+          <Col>
+            <Navbar bg="light" expand="lg">
+              <Navbar.Brand href="#home">React-Sudoku</Navbar.Brand>
+            </Navbar>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <DropdownButton id="dropdown-basic-button" title="Select Level">
+              <Dropdown.Item href="#" onClick={this.handleEasy}>
+                Easy
+              </Dropdown.Item>
+              <Dropdown.Item href="#" onClick={this.handleMedium}>
+                Medium
+              </Dropdown.Item>
+              <Dropdown.Item href="#" onClick={this.handleEasy}>
+                Hard
+              </Dropdown.Item>
+            </DropdownButton>
+
+            <span>Timer: {ms(this.state.time)}</span>
+
+            <Button onClick={this.startGame}>Start</Button>
+          </Col>
+        </Row>
+        <Row>
+          <Col>
+            <SudokuTable
+              questionGrid={this.state.questionGrid}
+              answerGrid={this.state.answerGrid}
+              stopTimer={this.stopGame}
+            />
+          </Col>
+        </Row>
+      </Container>
     );
   }
 }
--- src/components/SudokuTable.js
+++ src/components/SudokuTable.js
@@ -1,34 +1,47 @@
 import React, { Component } from "react";
-import { Button, Card } from "react-bootstrap";
-let solution = [
-  [3, 0, 6, 5, 0, 8, 4, 0, 0],
-  [5, 2, 0, 0, 0, 0, 0, 0, 0],
-  [0, 8, 7, 0, 0, 0, 0, 3, 1],
-  [0, 0, 3, 0, 1, 0, 0, 8, 0],
-  [9, 0, 0, 8, 6, 3, 0, 0, 5],
-  [0, 5, 0, 0, 9, 0, 6, 0, 0],
-  [1, 3, 0, 0, 0, 0, 2, 5, 0],
-  [0, 0, 0, 0, 0, 0, 0, 7, 4],
-  [0, 0, 5, 2, 0, 6, 3, 0, 0],
-];
+import { Button, Table, Dropdown, DropdownButton } from "react-bootstrap";
 
 class SudokuTable extends Component {
   constructor(props) {
     super(props);
+    this.inputBox = React.createRef();
+    this.solution = [
+      [3, 0, 6, 5, 0, 8, 4, 0, 0],
+      [5, 2, 0, 0, 0, 0, 0, 0, 0],
+      [0, 8, 7, 0, 0, 0, 0, 3, 1],
+      [0, 0, 3, 0, 1, 0, 0, 8, 0],
+      [9, 0, 0, 8, 6, 3, 0, 0, 5],
+      [0, 5, 0, 0, 9, 0, 6, 0, 0],
+      [1, 3, 0, 0, 0, 0, 2, 5, 0],
+      [0, 0, 0, 0, 0, 0, 0, 7, 4],
+      [0, 0, 5, 2, 0, 6, 3, 0, 0],
+    ];
 
     this.state = {
-      myGrid: solution,
+      myGrid: this.solution,
+      questionGrid: this.props.questionGrid,
+      theme: "",
     };
   }
+
+  componentDidUpdate = (prevProps) => {
+    if (this.props.questionGrid !== prevProps.questionGrid) {
+      this.setState({
+        myGrid: this.solution,
+        questionGrid: this.props.questionGrid,
+      });
+    }
+  };
 
   handleChange = (e) => {
     let row = parseInt(e.target.id.substring(10, 11));
     let col = parseInt(e.target.id.substring(12, 13));
     let val = parseInt(e.target.value);
     console.log(`the value of id ${e.target.id} is ${e.target.value}`);
-    solution[row][col] = val;
+    this.solution[row][col] = val;
+    let self = this;
     this.setState({
-      myGrid: solution,
+      myGrid: self.solution,
     });
   };
 
@@ -49,37 +62,76 @@ class SudokuTable extends Component {
     return;
   };
 
+  changeTheme1 = () => {
+    this.setState({
+      theme: "linear-gradient(to right, #355c7d, #6c5b7b, #c06c84)",
+    });
+  };
+
+  changeTheme2 = () => {
+    this.setState({
+      theme: "linear-gradient(to right, #2c3e50, #fd746c)",
+    });
+  };
+
+  changeTheme3 = () => {
+    this.setState({
+      theme: "linear-gradient(to right, #215f00, #e4e4d9)",
+    });
+  };
+
   render() {
     return (
       <React.Fragment>
-        <tbody>
-          {this.props.questionGrid.map((row, index, arr) => (
-            <tr>
-              {row.map((val, ind, array) => (
-                <td>
-                  {val === 0 ? (
-                    <input
-                      type="number"
-                      id={`cellcount-${index}-${ind}`}
-                      style={{ border: "1px solid #df9bde" }}
-                      onChange={this.handleChange}
-                      min={1}
-                      max={9}
-                    />
-                  ) : (
-                    <input
-                      type="number"
-                      value={val}
-                      id={`cellcount-${index}-${ind}`}
-                      style={{ border: "1px solid bisque" }}
-                      readOnly
-                    />
-                  )}
-                </td>
-              ))}
-            </tr>
-          ))}
-        </tbody>
+        <DropdownButton id="dropdown-basic-button" title="Choose Theme">
+          <Dropdown.Item href="#" onClick={this.changeTheme1}>
+            Red Sunset
+          </Dropdown.Item>
+          <Dropdown.Item href="#" onClick={this.changeTheme2}>
+            Dusk
+          </Dropdown.Item>
+          <Dropdown.Item href="#" onClick={this.changeTheme3}>
+            Misty Meadow
+          </Dropdown.Item>
+        </DropdownButton>
+        <Table
+          striped
+          bordered
+          hover
+          responsive
+          style={{ background: this.state.theme }}
+        >
+          <tbody>
+            {this.state.questionGrid.map((row, index, arr) => (
+              <tr key={index}>
+                {row.map((val, ind, array) => (
+                  <td key={ind}>
+                    {val === 0 ? (
+                      <input
+                        type="number"
+                        id={`cellcount-${index}-${ind}`}
+                        style={{ border: "1px solid #df9bde" }}
+                        onChange={this.handleChange}
+                        min={1}
+                        max={9}
+                        ref={this.inputBox}
+                      />
+                    ) : (
+                      <input
+                        type="number"
+                        value={val}
+                        id={`cellcount-${index}-${ind}`}
+                        style={{ border: "1px solid bisque" }}
+                        ref={this.inputBox}
+                        readOnly
+                      />
+                    )}
+                  </td>
+                ))}
+              </tr>
+            ))}
+          </tbody>
+        </Table>
         <Button onClick={this.checkGrid}> Finish</Button>
       </React.Fragment>
     );
