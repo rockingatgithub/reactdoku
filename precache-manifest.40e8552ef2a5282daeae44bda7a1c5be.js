a8511e508032f1fa0eaa62e87206e7e41acb4110
rockingatgithub <contactwithsid@gmail.com>
2020-10-04 18:29:31 +0530

    Updates

---
+++ precache-manifest.40e8552ef2a5282daeae44bda7a1c5be.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "41be247de5a6ad66bcb6a3dfa293cb26",
+    "url": "/reactdoku/index.html"
+  },
+  {
+    "revision": "1cd97d1545bfc7e26ed2",
+    "url": "/reactdoku/static/css/2.d9ad5f5c.chunk.css"
+  },
+  {
+    "revision": "eae9d61ae42091b43af9",
+    "url": "/reactdoku/static/css/main.4d121d93.chunk.css"
+  },
+  {
+    "revision": "1cd97d1545bfc7e26ed2",
+    "url": "/reactdoku/static/js/2.751d0b1f.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/reactdoku/static/js/2.751d0b1f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eae9d61ae42091b43af9",
+    "url": "/reactdoku/static/js/main.bab09794.chunk.js"
+  },
+  {
+    "revision": "224f436d2df468347047",
+    "url": "/reactdoku/static/js/runtime-main.0b70437d.js"
+  }
+]);
